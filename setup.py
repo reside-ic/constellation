43248d85506b6c8654f106a5d668b2c46680541b
Alex Hill <alex.hill@gmail.com>
2023-07-27 11:25:44 +0100

    increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
     "vault_dev"]
 
 setup(name="constellation",
-      version="1.0.0",
+      version="1.1.0",
       description="Deploy scripts for constellations of docker containers",
       long_description=long_description,
       classifiers=[
