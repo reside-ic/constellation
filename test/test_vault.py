d1354c0c23364324650f1ab46d67746f3fb9c5c9
Mantra <mantrakusumgar@outlook.com>
2023-10-13 14:42:49 +0100

    skipping tests instead of deleting them

--- test/test_vault.py
+++ test/test_vault.py
@@ -104,6 +104,49 @@ def test_vault_config_when_missing():
         cl.read("secret/foo")
 
 
+# To run this test you will need a token for the vimc robot user -
+# this can be found in the vimc vault as
+# /secret/vimc-robot/github-pat
+#
+# This environment variable is configured on GitHub actions see usage details
+# https://mrc-ide.myjetbrains.com/youtrack/articles/RESIDE-A-18/Vault#keys
+def test_vault_config_login():
+    pytest.skip("Skipping test temporary, ticket to resolve this:"
+                + "https://mrc-ide.myjetbrains.com/youtrack/issue"
+                + "/RESIDE-351/Vault-login-testing-with-GitHub-authentication")
+
+    if "VAULT_TEST_GITHUB_PAT" not in os.environ:
+        pytest.skip("VAULT_TEST_GITHUB_PAT is not defined")
+    with vault_dev.server() as s:
+        cl = s.client()
+        cl.sys.enable_auth_method(method_type="github")
+        cl.write("auth/github/config", organization="vimc")
+
+        url = "http://localhost:{}".format(s.port)
+        token = os.environ["VAULT_TEST_GITHUB_PAT"]
+        cfg = vault_config(url, "github", {"token": token})
+        assert cfg.client().is_authenticated()
+
+
+def test_vault_config_login_no_args():
+    pytest.skip("Skipping test temporary, ticket to resolve this:"
+                + "https://mrc-ide.myjetbrains.com/youtrack/issue"
+                + "/RESIDE-351/Vault-login-testing-with-GitHub-authentication")
+
+    if "VAULT_TEST_GITHUB_PAT" not in os.environ:
+        pytest.skip("VAULT_TEST_GITHUB_PAT is not defined")
+
+    with vault_dev.server() as s:
+        cl = s.client()
+        cl.sys.enable_auth_method(method_type="github")
+        cl.write("auth/github/config", organization="vimc")
+        url = "http://localhost:{}".format(s.port)
+        token = os.environ["VAULT_TEST_GITHUB_PAT"]
+        with mock.patch.dict(os.environ, {"VAULT_AUTH_GITHUB_TOKEN": token}):
+            cfg = vault_config(url, "github", None)
+            assert cfg.client().is_authenticated()
+
+
 # Utility required to work around https://github.com/hvac/hvac/issues/421
 def test_drop_envvar_removes_envvar():
     name = "VAULT_DEV_TEST_VAR"
