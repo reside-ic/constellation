1b18cf1c74094c7087034e88e10da8087869603c
Mantra <mantrakusumgar@outlook.com>
2023-10-13 13:32:31 +0100

    lint

--- test/test_constellation.py
+++ test/test_constellation.py
@@ -406,14 +406,16 @@ def test_can_preconfigure_constellation_containers():
     arg_container = ["sleep", "1000"]
 
     def precfg_container(container, data):
-        docker_util.string_into_container("test string", container, "./test.txt")
+        docker_util.string_into_container("test string", container,
+                                          "./test.txt")
 
     def cfg_container(container, data):
         res = container.exec_run(["cat", "test.txt"])
         assert res.output.decode("utf-8") == "test string"
 
-    client = ConstellationContainer("client", ref_container, arg_container,
-                                    configure=cfg_container, preconfigure=precfg_container)
+    client = ConstellationContainer("client", ref_container,
+                                    arg_container, configure=cfg_container,
+                                    preconfigure=precfg_container)
 
     obj = Constellation(name, prefix, [client], network, volumes)
     obj.start()
