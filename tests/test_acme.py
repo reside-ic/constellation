6ee8102acad8813761bb0992db434b969332f070
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-11-12 17:20:55 +0000

    Test acme args

--- tests/test_acme.py
+++ tests/test_acme.py
@@ -1,6 +1,8 @@
+import types
+
 import pytest
 
-from constellation.acme import acme_buddy_env
+from constellation.acme import acme_buddy_container, acme_buddy_env
 from constellation.config import config_acme
 
 
@@ -92,3 +94,53 @@ def test_acme_buddy_bad_provider():
         ValueError, match="Unrecognised DNS provider: anotherdns"
     ):
         _env = acme_buddy_env("anotherdns", data)
+
+
+def test_acme_buddy_container(monkeypatch):
+    monkeypatch.setenv("ACME_BUDDY_STAGING", "0")
+    data = {
+        "acme_buddy": {
+            "additional_domains": ["anotherhost.com"],
+            "email": "reside@imperial.ac.uk",
+            "image": {
+                "repo": "ghcr.io/reside-ic",
+                "name": "acme-buddy",
+                "tag": "main",
+            },
+            "port": 2112,
+            "dns_provider": "cloudflare",
+            "env": {
+                "CLOUDFLARE_DNS_API_TOKEN": "abcdefgh12345678",
+            },
+        }
+    }
+
+    acme_cfg = config_acme(data)
+    cfg = types.SimpleNamespace()
+    cfg.containers = {"acme-buddy": "acme-buddy"}
+    cfg.container_prefix = "prefix"
+    cfg.hostname = "example.com"
+    cfg.acme_buddy = types.SimpleNamespace(
+        dns_provider = "cloudflare",
+        cloudflare_token = "abcdefgh12345678",
+        ref = "ghcr.io/reside-ic/acme-buddy:main",
+        port = 2112,
+        email = "reside@imperial.ac.uk",
+        additional_domains=["www.example.com"],
+    )
+
+    proxy = types.SimpleNamespace()
+    proxy.name_external = lambda prefix: f"{prefix}-proxy"
+    tls_volume = "tls-volume"
+    acme = acme_buddy_container(cfg, proxy, tls_volume)
+
+    assert acme.environment["ACME_BUDDY_STAGING"] == 0
+    assert acme.environment["CLOUDFLARE_DNS_API_TOKEN"] == "abcdefgh12345678"
+    assert acme.args[0] == "--domain"
+    assert acme.args[1] == "example.com,www.example.com"
+    assert acme.args[2] == "--email"
+    assert acme.args[3] == "reside@imperial.ac.uk"
+    assert acme.args[4] == "--dns-provider"
+    assert acme.args[5] == "cloudflare"
+    assert acme.args[-1] == "prefix-proxy"
+
