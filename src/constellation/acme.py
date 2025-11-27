55922fb0f98424ac7e98be96e0989ceeae1fad4a
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-11-27 16:52:23 +0000

    Take env instead of repopulating it

--- src/constellation/acme.py
+++ src/constellation/acme.py
@@ -1,7 +1,7 @@
 import os
 
 import constellation
-from constellation.config import config_integer, config_list, config_string
+from constellation.config import config_dict, config_integer, config_list, config_string
 
 
 class AcmeBuddyConfig:
@@ -10,25 +10,11 @@ class AcmeBuddyConfig:
         repo = config_string(data, ["acme_buddy", "image", "repo"])
         tag = config_string(data, ["acme_buddy", "image", "tag"])
         self.ref = constellation.ImageReference(repo, name, tag)
-
         self.port = config_integer(data, ["acme_buddy", "port"])
         self.dns_provider = config_string(data, ["acme_buddy", "dns_provider"])
-
-        if self.dns_provider == "hdb":
-            self.hdb_username = config_string(
-                data, ["acme_buddy", "env", "HDB_ACME_USERNAME"]
-            )
-            self.hdb_password = config_string(
-                data, ["acme_buddy", "env", "HDB_ACME_PASSWORD"]
-            )
-        elif self.dns_provider == "cloudflare":
-            self.cloudflare_token = config_string(
-                data, ["acme_buddy", "env", "CLOUDFLARE_DNS_API_TOKEN"]
-            )
-        else:
-            err = f"Unrecognised DNS provider: {self.dns_provider}"
-            raise ValueError(err)
-
+        self.env = config_dict(data, ["acme_buddy", "env"])
+        if "ACME_BUDDY_STAGING" in os.environ:
+            self.env["ACME_BUDDY_STAGING"] = os.environ["ACME_BUDDY_STAGING"]
         self.email = config_string(data, ["acme_buddy", "email"])
         if "additional_domains" in data["acme_buddy"]:
             self.additional_domains = config_list(
@@ -36,31 +22,9 @@ class AcmeBuddyConfig:
             )
 
 
-def acme_buddy_env(dns_provider, acme_buddy_cfg):
-    staging = int(os.environ.get("ACME_BUDDY_STAGING", "0"))
-    acme_env = {"ACME_BUDDY_STAGING": staging}
-
-    if dns_provider == "hdb":
-        acme_env.update(
-            {
-                "HDB_ACME_USERNAME": acme_buddy_cfg.hdb_username,
-                "HDB_ACME_PASSWORD": acme_buddy_cfg.hdb_password,
-            }
-        )
-    elif dns_provider == "cloudflare":
-        acme_env.update(
-            {"CLOUDFLARE_DNS_API_TOKEN": acme_buddy_cfg.cloudflare_token}
-        )
-    else:
-        err = f"Unrecognised DNS provider: {dns_provider}"
-        raise ValueError(err)
-    return acme_env
-
-
 def acme_buddy_container(cfg, proxy, tls_volume):
     name = cfg.containers["acme-buddy"]
     dns_provider = cfg.acme_buddy.dns_provider
-    acme_env = acme_buddy_env(dns_provider, cfg.acme_buddy)
 
     acme_mounts = [
         constellation.ConstellationVolumeMount(tls_volume, "/tls"),
@@ -77,7 +41,7 @@ def acme_buddy_container(cfg, proxy, tls_volume):
         cfg.acme_buddy.ref,
         ports=[cfg.acme_buddy.port],
         mounts=acme_mounts,
-        environment=acme_env,
+        environment=cfg.acme_buddy.env,
         args=[
             "--domain",
             domain_names,
--- tests/test_acme.py
+++ tests/test_acme.py
@@ -2,7 +2,7 @@ import types
 
 import pytest
 
-from constellation.acme import acme_buddy_container, acme_buddy_env
+from constellation.acme import acme_buddy_container
 from constellation.config import config_acme
 
 
@@ -27,18 +27,16 @@ def test_acme_buddy_config_hdb():
     cfg = config_acme(data)
     assert cfg.email == "reside@imperial.ac.uk"
     assert cfg.dns_provider == "hdb"
-    assert cfg.hdb_username == "testuser"
-    assert cfg.hdb_password == "testpw"
     assert cfg.ref.repo == "ghcr.io/reside-ic"
     assert cfg.ref.name == "acme-buddy"
     assert cfg.ref.tag == "main"
     assert cfg.port == 2112
-    env = acme_buddy_env(cfg.dns_provider, cfg)
-    assert env["HDB_ACME_USERNAME"] == cfg.hdb_username
-    assert env["HDB_ACME_PASSWORD"] == cfg.hdb_password
+    assert cfg.env["HDB_ACME_USERNAME"] == "testuser"
+    assert cfg.env["HDB_ACME_PASSWORD"] == "testpw"
 
 
-def test_acme_buddy_config_cloudflare():
+def test_acme_buddy_config_cloudflare(monkeypatch):
+    monkeypatch.setenv("ACME_BUDDY_STAGING", "0")
     data = {
         "acme_buddy": {
             "additional_domains": ["anotherhost.com"],
@@ -58,57 +56,31 @@ def test_acme_buddy_config_cloudflare():
     cfg = config_acme(data)
     assert cfg.email == "reside@imperial.ac.uk"
     assert cfg.dns_provider == "cloudflare"
-    assert cfg.cloudflare_token == "abcdefgh12345678"
     assert cfg.ref.repo == "ghcr.io/reside-ic"
     assert cfg.ref.name == "acme-buddy"
     assert cfg.ref.tag == "main"
     assert cfg.port == 2112
     assert cfg.additional_domains == ["anotherhost.com"]
-    env = acme_buddy_env(cfg.dns_provider, cfg)
-    assert env["CLOUDFLARE_DNS_API_TOKEN"] == cfg.cloudflare_token
+    assert cfg.env["CLOUDFLARE_DNS_API_TOKEN"] == "abcdefgh12345678"
+    assert cfg.env["ACME_BUDDY_STAGING"] == '0'
 
 
-def test_acme_buddy_bad_provider():
-    data = {
-        "acme_buddy": {
-            "additional_domains": ["anotherhost.com"],
-            "email": "reside@imperial.ac.uk",
-            "image": {
-                "repo": "ghcr.io/reside-ic",
-                "name": "acme-buddy",
-                "tag": "main",
-            },
-            "port": 2112,
-            "dns_provider": "anotherdns",
-            "env": {
-                "ANOTHER_API_TOKEN": "abcdefgh12345678",
-            },
-        },
-    }
-    with pytest.raises(
-        ValueError, match="Unrecognised DNS provider: anotherdns"
-    ):
-        _cfg = config_acme(data)
-
-    with pytest.raises(
-        ValueError, match="Unrecognised DNS provider: anotherdns"
-    ):
-        _env = acme_buddy_env("anotherdns", data)
-
-
-def test_acme_buddy_container(monkeypatch):
-    monkeypatch.setenv("ACME_BUDDY_STAGING", "0")
+def test_acme_buddy_container():
     cfg = types.SimpleNamespace()
     cfg.containers = {"acme-buddy": "acme-buddy"}
     cfg.container_prefix = "prefix"
     cfg.hostname = "example.com"
     cfg.acme_buddy = types.SimpleNamespace(
         dns_provider="cloudflare",
-        cloudflare_token="abcdefgh12345678",
+        env={
+            "CLOUDFLARE_DNS_API_TOKEN": "abcdefgh12345678", 
+            "ACME_BUDDY_STAGING": "0",
+        },
         ref="ghcr.io/reside-ic/acme-buddy:main",
         port=2112,
         email="reside@imperial.ac.uk",
         additional_domains=["www.example.com"],
+        
     )
 
     proxy = types.SimpleNamespace()
@@ -116,7 +88,7 @@ def test_acme_buddy_container(monkeypatch):
     tls_volume = "tls-volume"
     acme = acme_buddy_container(cfg, proxy, tls_volume)
 
-    assert acme.environment["ACME_BUDDY_STAGING"] == 0
+    assert acme.environment["ACME_BUDDY_STAGING"] == '0'
     assert acme.environment["CLOUDFLARE_DNS_API_TOKEN"] == "abcdefgh12345678"
     assert acme.args[0] == "--domain"
     assert acme.args[1] == "example.com,www.example.com"
